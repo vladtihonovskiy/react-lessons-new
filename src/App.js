5bd93af244b560b177863ad4e9414c56f88932ec
vlad tihonovskiy <balyko.peaceit@gmail.com>
2018-08-30 19:23:52 +0300

    Refs brunch create

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		//this.textIputSmartInput = React.createRef();
+		// create a ref to store the textInput DOM element
+	}
+
+	focusTextInput = () => {
+		// Explicitly focus the text input using the raw DOM API
+		// // Note: we're accessing "current" to get the DOM node
+		// console.log(this.refs);
+		// this.refs.textInput.focus();
+		//
+		// console.log(this.refs.textInput.value)
+		// //this.textIputSmartInput.current.focus();
+	}
+
+	render() {
+		// tell React that we want to associate the <input> ref
+		// with the `textInput` that we created in the constructor
+		return (
+			<div>
+				<input
+					type="text"
+					ref={"textInput"} />
+
+				<input
+					type="button"
+					value="Focus the text input"
+					onClick={this.focusTextInput}
+				/>
+			</div>
+		);
+	}
 }
-
-export default App;
+export default App
