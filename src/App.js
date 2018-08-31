cae9d3fe043ee0c2910eb80e25c219113afe6563
vlad tihonovskiy <balyko.peaceit@gmail.com>
2018-08-31 10:56:28 +0300

    Add refs

--- src/App.js
+++ src/App.js
@@ -7,29 +7,60 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		//this.textIputSmartInput = React.createRef();
+		this.textInputSmartInputCreating = React.createRef();
+
+		this.text = "";
 		// create a ref to store the textInput DOM element
+		//this.textInputSmartInput = 5;
+	}
+
+	state = {
+		text: ""
+	}
+
+	componentDidMount() {
+		this.refs.textInputSmartInput.value = 15;
+		this.refs.textInputSmartInput.focus();
+
+		this.setState({
+			text:  this.refs.textInputSmartInput.value
+		});
+
+		this.textInputSmartInputCreating.current.value = "Reaf save on constructor";
 	}
 
 	focusTextInput = () => {
+
+		console.log(this.refs);
+
+		this.setState({
+			text:  this.refs.textInputSmartInput.value
+		});
 		// Explicitly focus the text input using the raw DOM API
 		// // Note: we're accessing "current" to get the DOM node
 		// console.log(this.refs);
 		// this.refs.textInput.focus();
-		//
-		// console.log(this.refs.textInput.value)
-		// //this.textIputSmartInput.current.focus();
+
+		this.textInputSmartInputCreating.current.value = this.refs.textInputSmartInput.value;
 	}
 
 	render() {
 		// tell React that we want to associate the <input> ref
 		// with the `textInput` that we created in the constructor
+		//console.log(this.textInputSmartInput);
 		return (
 			<div>
 				<input
 					type="text"
-					ref={"textInput"} />
+					onChange={this.focusTextInput}
+					ref={"textInputSmartInput"} />
 
+				<input
+					type="text"
+					onChange={this.focusTextInput}
+					ref={this.textInputSmartInputCreating} />
+
+				<input type="text" value={this.state.text} />
 				<input
 					type="button"
 					value="Focus the text input"
