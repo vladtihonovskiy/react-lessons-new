4cfedbd426dbfcf1f8efa75fe150b3d429a913f8
vlad tihonovskiy <balyko.peaceit@gmail.com>
2018-09-11 21:24:48 +0300

    add loader

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactDOM from "react-dom";
-
-import Children from "./component/Children";
-
+import Loader from "./component/Loader";
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
-			text: ''
+			isLoading: true
 		}
 	}
 
-	handlePrintText(text) {
-		this.setState({
-			text: text
-		});
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				isLoading: false
+			});
+		},2000)
 	}
 
 	render() {
-		return(
+		const { isLoading } = this.state;
+		return (
 			<div>
-				<h1>Test</h1>
-				<div style={{padding: '10px 20px', width: '300px', textAlign: 'center', border: '1px dashed #cccccc'}}>{(this.state.text!='') ? this.state.text : <span>&nbsp;</span>}</div>
-				<br />
-				<Children
-					text={this.state.text}
-					onPrintText={this.handlePrintText.bind(this)}
-				/>
+				{isLoading ? <Loader /> : "Hi"}
 			</div>
 		);
 	}
