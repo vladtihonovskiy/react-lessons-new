0bd35dbda343b5946dbece38a317b3e8c69f9bc1
vlad tihonovskiy <balyko.peaceit@gmail.com>
2018-10-09 19:06:23 +0300

    add loader component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import NavBar from "./components/NavBar/NavBar";
 
 import classes from './App.less';
 
+import { LoaderContainer, loader } from "./components/Loader/Loader";
+
 import { apiUrl } from "./constants/config";
 
 class App extends Component {
@@ -11,9 +13,17 @@ class App extends Component {
     render() {
         return (
             <div className={classes.app}>
+				<LoaderContainer />
                 <NavBar />
                 <div className={classes.appContainer}>
                     123123123123123
+
+					<button onClick={()=> {
+						loader.show();
+					}}>Show</button>
+					<button onClick={()=> {
+						loader.hide();
+					}}>Hide</button>
                 </div>
             </div>
     );
